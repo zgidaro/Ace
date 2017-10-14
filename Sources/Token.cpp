c3a6cf7c48b4a954effa5184b462193d201952d9
Elias <elias.t.gonzalez@gmail.com>
2017-10-14 19:10:20 -0400

    Set up Token class.

--- Sources/Token.cpp
+++ Sources/Token.cpp
@@ -16,11 +16,11 @@ Token::Token(int _column, char _row, char _colour) {
     this->setColour(_colour);
 }
 
-Token::~Token() {}
+Token::~Token() = default;
 
-Token::setColumn(int _column) {column = _column;}
-Token::setRow(char _row) {row = _row;}
-Token::setColour(char _colour) {colour = _colour;}
+void Token::setColumn(int _column) {column = _column;}
+void Token::setRow(char _row) {row = _row;}
+void Token::setColour(char _colour) {colour = _colour;}
 
 int Token::getColumn() {return column;}
 char Token::getRow() {return row;}
