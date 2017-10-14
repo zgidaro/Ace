7dda5f749532989dfe3ccf7b433ba442d8e7c22d
Elias <elias.t.gonzalez@gmail.com>
2017-10-14 19:08:55 -0400

    Set up Token class.

--- Headers/Token.h
+++ Headers/Token.h
@@ -5,12 +5,30 @@
 #ifndef ACE_TOKEN_H
 #define ACE_TOKEN_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 class Token {
 private:
+    int column;
+    char row;
+    char colour;
+
 
 public:
+    Token();
+    Token(int, char, char);
+    ~Token();
 
+    int getColumn();
+    char getRow();
+    char getColour();
+
+    void setColumn(int);
+    void setRow(char);
+    void setColour(char);
 };
 
 
--- Sources/Token.cpp
+++ Sources/Token.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "../Headers/Token.h"
+
+Token::Token() {
+    column = 0;
+    row = '';
+    colour = '';
+}
+
+Token::Token(int _column, char _row, char _colour) {
+    this->setColumn(_column);
+    this->setRow(_row);
+    this->setColour(_colour);
+}
+
+Token::~Token() {}
+
+Token::setColumn(int _column) {column = _column;}
+Token::setRow(char _row) {row = _row;}
+Token::setColour(char _colour) {colour = _colour;}
+
+int Token::getColumn() {return column;}
+char Token::getRow() {return row;}
+char Token::getColour() {return colour;}
