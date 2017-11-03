0fc89285e7e4e1d9603ad92d01dc465c955b571c
zgidaro <zgidaro@live.com>
2017-11-02 22:56:18 -0400

    Added Player class.

---
+++ Headers/Player.h
@@ -0,0 +1,11 @@
+#ifndef ACE_PLAYER_H
+#define ACE_PLAYER_H
+
+
+class Player
+{
+
+};
+
+
+#endif //ACE_PLAYER_H
---
+++ Sources/Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexander Gidaro on 2017-11-02.
+//
+
+#include "../Headers/Player.h"
