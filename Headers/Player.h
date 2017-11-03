5f7f16adacf525e1f2f543fb4ab21562e5377b0e
zgidaro <zgidaro@live.com>
2017-11-02 23:05:42 -0400

    Tree/Node structure

--- Headers/Player.h
+++ Headers/Player.h
@@ -1,10 +1,30 @@
 #ifndef ACE_PLAYER_H
 #define ACE_PLAYER_H
 
+struct Node
+{
+	Node ** child;
+	int count;
+	int heuristic;
+	/*
+	 * e(board) = 100 x SUM(horizontal index g) from g = 1 to # green tokens on board
+	 * 			  + 50  x SUM(vertical index g) from g = 1 to # green tokens on board
+	 * 			  - 100 x SUM(horizontal index r) from r = 1 to # red tokens on board
+	 * 			  - 50  x SUM(vertical index r) from r = 1 to # red tokens on board
+	 */
+};
 
 class Player
 {
+private:
+	Node * tree;
 
+public:
+	Player();
+	~Player();
+
+	void setTree(Node *);
+	Node * getTree();
 };
 
 
